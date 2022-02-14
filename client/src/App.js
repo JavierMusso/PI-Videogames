0177e02fd07f7909cf09719b3e538a488094a89b
JavierMusso <javier.musso96@gmail.com>
2022-02-14 17:10:10 -0300

    frontend setup, routes done and css resetted

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Create from "./components/Create/Create";
+import GameDetail from "./components/GameDetail/GameDetail";
+import Home from "./components/Home/Home";
+import Landing from "./components/Landing/Landing";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Videogames</h1>
+      <Routes>
+        <Route path="/*" element={<NotFound />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/game/:id" element={<GameDetail />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/components/GameDetail/GameDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GameDetail() {
+  return <h1>GameDetail</h1>;
+}
+
+export default GameDetail;
