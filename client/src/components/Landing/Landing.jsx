2edfa96564697a0bc4b2572872101693313980eb
JavierMusso <javier.musso96@gmail.com>
2022-02-23 04:34:07 -0300

    adds styles to landing page

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./Landing.module.css";
 
 function Landing() {
   return (
-    <div className="Landing">
-      <h1>Welcome Lorem ipsum dolor sit amet.</h1>
-      <Link to="/home">Start</Link>
+    <div className={styles.Landing}>
+      <div className={styles.border}>
+        <div>
+          <h1>
+            Welcome, Player1 <br /> <span>Find what to play next... </span>
+          </h1>
+          <Link to="/home">Let's Go!</Link>
+        </div>
+      </div>
     </div>
   );
 }
