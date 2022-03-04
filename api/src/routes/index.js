dad273a3e6cb115852559b1408842ec1529f9c4b
JavierMusso <javier.musso96@gmail.com>
2022-03-03 21:33:54 -0300

    adds suport for default image at my backend

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,10 +2,15 @@ const axios = require("axios");
 const { Router } = require("express");
 const genresRoutes = require("./genres");
 const videogamesRoutes = require("./videogames");
+const fs = require("fs");
 
 const router = Router();
 
 router.use("/videogames", videogamesRoutes);
 router.use("/genres", genresRoutes);
+
+router.get("/default-game.jpg", (req, res) => {
+  res.sendFile("default-game.jpg", { root: __dirname });
+});
 
 module.exports = router;
